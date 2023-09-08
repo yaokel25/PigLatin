759ef40e257a08da21f3e8ac1500f3d3ae6cd7ef
yaokel25 <yaokel25@7hills.org>
2023-09-08 13:10:08 -0400

    made if statement

--- PigLatin.java
+++ PigLatin.java
@@ -6,6 +6,7 @@ public class PigLatin{
     System.out.print("Phrase: ");
     String phrase = scan.nextLine();
     int len = phrase.length();
+    if (phrase[0] = a || phrase[0])
     phrase.substring(i, i+1);//up to but not including i +1
 
     }
