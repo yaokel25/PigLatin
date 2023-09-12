d51ffed818860bebe74738d404297c5637aa3e85
yaokel25 <yaokel25@7hills.org>
2023-09-11 21:20:31 -0400

    used string.substring

--- PigLatin.java
+++ PigLatin.java
@@ -6,16 +6,15 @@ public class PigLatin{
     System.out.print("Phrase: ");
     String phrase = scan.nextLine();
     int len = phrase.length();
-    if (phrase[0] = a || phrase[0]){
+    if (phrase[0] == "a" || phrase[0] == "e" || phrase[0] == "o" || phrase[0] == "u" || phrase[0] == "i"){
     phrase = phrase + "ay";
     System.out.print(phrase);
     }
     else {
         char firstLetter = phrase[0];
-    for(int i = 0; i < len; i++){
-        char placeHolder = phrase[i+1];
-        phrase[i] = placeHolder;
-    }
+        phrase = phrase.substring(1, len+1)；
+
+    
     phrase = phrase + firstLetter + "ay";
     System.out.print(phrase);
     }
