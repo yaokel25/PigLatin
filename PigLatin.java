aa8377566980d0c486af46800b2b11da220dfdec
yaokel25 <yaokel25@7hills.org>
2023-09-11 20:53:01 -0400

    tried to do one word

--- PigLatin.java
+++ PigLatin.java
@@ -6,7 +6,19 @@ public class PigLatin{
     System.out.print("Phrase: ");
     String phrase = scan.nextLine();
     int len = phrase.length();
-    if (phrase[0] = a || phrase[0])
+    if (phrase[0] = a || phrase[0]){
+    phrase = phrase + "ay";
+    System.out.print(phrase);
+    }
+    else {
+        char firstLetter = phrase[0];
+    for(int i = 0; i < len; i++){
+        phrase[i] = phrase [i+1]；
+    }
+    phrase = phrase + firstLetter + "ay";
+    System.out.print(phrase);
+    }
+
     phrase.substring(i, i+1);//up to but not including i +1
 
     }
