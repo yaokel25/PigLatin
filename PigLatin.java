ebfcd84015a71ecc9fbd07afc11d73737996b6bc
yaokel25 <yaokel25@7hills.org>
2023-09-11 21:25:12 -0400

    don't know how to get char from string phrase with out changing string to array

--- PigLatin.java
+++ PigLatin.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class PigLatin{
     public static void main(String[] args){ 
     Scanner scan = new Scanner(System.in);
-    System.out.print("Phrase: ");
     String phrase = scan.nextLine();
+    System.out.print("Phrase: ");
     int len = phrase.length();
     if (phrase[0] == "a" || phrase[0] == "e" || phrase[0] == "o" || phrase[0] == "u" || phrase[0] == "i"){
     phrase = phrase + "ay";
@@ -12,7 +12,7 @@ public class PigLatin{
     }
     else {
         char firstLetter = phrase[0];
-        phrase = phrase.substring(1, len+1)；
+        phrase = phrase.substring(1, len);
 
     
     phrase = phrase + firstLetter + "ay";
